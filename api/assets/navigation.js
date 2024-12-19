e28b9b90c9a3cd29ee5253b600717e1600698313
MasatoMakino <MasatoMakino@users.noreply.github.com>
2024-12-19 06:38:14 +0000

    deploy: 766b72c85a40e02e524116723cee84f3798c46dd

--- api/assets/navigation.js
+++ api/assets/navigation.js
@@ -1,1 +1,1 @@
-window.navigationData = "data:application/octet-stream;base64,H4sIAAAAAAAAA4uuVipJrShRslIKyC/OLMnMzwstycxR0lEqSCzJULJSSs5JLC5OLdZHltXLKMkFKcnOzEtRsjI0sqiNBQDpmGUDRwAAAA=="
+window.navigationData = "eJyLrlYqSa0oUbJSCsgvzizJzM8LLcnMUdJRKkgsyVCyUkrOSSwuTi3WR5bVyyjJBSnJzsxLUbIyNLKojQUAh1AYpA=="
