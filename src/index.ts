3ed8f47470ae57fffda4fdb31811f5afc85d7a31
MasatoMakino <makino.masato.g@gmail.com>
2025-06-19 14:38:35 +0900

    docs: add deprecation notice for PositionUtil export

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,7 @@
 export * from "./threejs-position-util.js";
 
 import * as PositionUtil from "./threejs-position-util.js";
+/**
+ * @deprecated Use named exports directly from "@masatomakino/threejs-position-util" instead.
+ */
 export { PositionUtil };
